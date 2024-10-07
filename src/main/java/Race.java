a41432c957b1ea8f9cab8cf04d5554e500315e77
Amazing_salami <bengami-n@yandex.ru>
2024-10-08 01:30:47 +0700

    First work commit

---
+++ src/main/java/Race.java
@@ -0,0 +1,18 @@
+public class Race {
+
+    private String leader = "";
+
+    private int distance = 0;
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public void addRacer(String name, int speed) {
+        int newDistance = speed * 24;
+        if (distance < newDistance) {
+            distance = newDistance;
+            leader = name;
+        }
+    }
+}
