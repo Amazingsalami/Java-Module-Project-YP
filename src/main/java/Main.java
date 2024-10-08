c14d2131c28589b71968019f4b1f0184156626b5
Amazing_salami <bengami-n@yandex.ru>
2024-10-08 13:47:58 +0700

    Second work commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,9 @@ public class Main {
         Race race = new Race();
 
         for (int i = 0; i < 3; i++) {
+
             System.out.println("- Введите название машины №" + (i + 1) + ":");
+
             String carName = scanner.next();
 
             int carSpeed;
@@ -16,7 +18,9 @@ public class Main {
                 System.out.println("- Введите скорость машины №" + (i + 1) + ":");
 
                 if (scanner.hasNextInt()) {
+
                     carSpeed = scanner.nextInt();
+
                     if (carSpeed > 0 && carSpeed <= 250) {
                         break;
                     } else {
